865ec0469d2b969ed6255efe0be68712db8ab7e8
Christine Angeles <angelescc@vcu.edu>
2022-03-02 22:54:15 -0500

    Adding on click listenter to splash activity

--- app/src/main/java/com/example/a475_uxui/SplashActivity.java
+++ app/src/main/java/com/example/a475_uxui/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.example.a475_uxui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              openHome();
+            }
+        });
+    }
+    public void openHome() {
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
     }
 }
