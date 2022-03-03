25e43c9d8c68498acce10e27cef62fb892ce5e24
Christine Angeles <angelescc@vcu.edu>
2022-03-02 21:11:19 -0500

    Creating main game page

--- app/src/main/java/com/example/a475_uxui/GamePage.java
+++ app/src/main/java/com/example/a475_uxui/GamePage.java
@@ -1,14 +1,22 @@
 package com.example.a475_uxui;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class GamePage extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
     }
 }
