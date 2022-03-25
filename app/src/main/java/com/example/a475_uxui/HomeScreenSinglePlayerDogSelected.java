b68d7f0da01d44ecffd733a727a76295cea1739d
Meagan Nguyen <nguyenm26@vcu.edu>
2022-03-25 00:49:04 -0400

    added pig avatar selection (single player)

--- app/src/main/java/com/example/a475_uxui/HomeScreenSinglePlayerDogSelected.java
+++ app/src/main/java/com/example/a475_uxui/HomeScreenSinglePlayerDogSelected.java
@@ -10,11 +10,20 @@ import android.widget.ImageView;
 public class HomeScreenSinglePlayerDogSelected extends AppCompatActivity {
 
     private ImageView start_button;
+    private ImageView pig_avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen_single_player_dog_selected);
+
+        pig_avatar = (ImageView) findViewById(R.id.pig_avatar);
+        pig_avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPig();
+            }
+        });
 
         start_button = (ImageView) findViewById(R.id.start_button);
         start_button.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +36,11 @@ public class HomeScreenSinglePlayerDogSelected extends AppCompatActivity {
 
     public void openGame() {
         Intent intent = new Intent(this, GamePage.class);
+        startActivity(intent);
+    }
+
+    public void selectPig() {
+        Intent intent = new Intent(this, HomeScreenSinglePlayerPigSelected.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/a475_uxui/HomeScreenSinglePlayerPigSelected.java
+++ app/src/main/java/com/example/a475_uxui/HomeScreenSinglePlayerPigSelected.java
@@ -2,13 +2,45 @@ package com.example.a475_uxui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeScreenSinglePlayerPigSelected extends AppCompatActivity {
+
+    private ImageView start_button;
+    private ImageView dog_avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen_single_player_pig_selected);
+
+        dog_avatar = (ImageView) findViewById(R.id.dog_avatar);
+        dog_avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectDog();
+            }
+        });
+
+        start_button = (ImageView) findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGame();
+            }
+        });
+    }
+
+    public void openGame() {
+        Intent intent = new Intent(this, GamePage.class);
+        startActivity(intent);
+    }
+
+    public void selectDog(){
+        Intent intent = new Intent(this, HomeScreenSinglePlayerDogSelected.class);
+        startActivity(intent);
     }
 }
